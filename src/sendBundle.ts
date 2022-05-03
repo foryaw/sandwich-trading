d4d3f04c59ad2a4a55a3c201fd64734061600ef1
foryaw <78723492+foryaw@users.noreply.github.com>
2022-05-03 18:27:59 +0800

    WIP 3/5

--- src/sendBundle.ts
+++ src/sendBundle.ts
@@ -2,13 +2,10 @@ import * as dotenv from "dotenv"
 dotenv.config()
 import { BigNumber, Wallet, ethers, Signer} from "ethers";
 import { FlashbotsBundleProvider, FlashbotsBundleRawTransaction, FlashbotsBundleTransaction } from "@flashbots/ethers-provider-bundle";
-import { TransactionRequest, TransactionResponse} from "@ethersproject/abstract-provider";
-import { GWEI, getDefaultRelaySigningKey, getRawTransaction} from "./utils";
+import { GWEI, getDefaultRelaySigningKey, getSignedTransaction } from "./utils";
 
 // flashbots uses goreli testnet
-const CHAIN_ID = 5;
-const FLAHSBOTS_ENDPOINT = "http://relay-goerli.flashbots.net";
-const url = "wss://mainnet.infura.io/ws/v3/fa688e4b59db4b4ba09be0235cc4a2b5";
+const NETWORK = "goerli"
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const FLASHBOTS_RELAY_SIGNING_KEY = process.env.FLASHBOTS_RELAY_SIGNING_KEY || getDefaultRelaySigningKey()
@@ -17,55 +14,12 @@ if(process.env.PRIVATE_KEY === "") {
     console.error("Please provide PRIVATE_KEY environment variable")
     process.exit(1);
 }
-if(process.env.FLASHBOTS_REPLAY_SIGNING_KEY === "") {
-    console.error("Please provide FLASHBOTS_REPLAY_SIGNING_KEY environment variable")
-    process.exit(1);
-}
 
-const provider = new ethers.providers.WebSocketProvider(url);
-console.log(PRIVATE_KEY)
-console.log(FLASHBOTS_RELAY_SIGNING_KEY)
-const arbitrageSigningWallet = new Wallet(PRIVATE_KEY)
+const provider = ethers.getDefaultProvider(process.env.GOERLI_RPC_URL);
+const arbitrageSigningWallet = new Wallet(PRIVATE_KEY, provider)
 const flashbotsRelaySigningWallet = new Wallet(FLASHBOTS_RELAY_SIGNING_KEY)
 
-async function getSignedTransaction(transaction: TransactionResponse) {
-    console.log("Getting signed transaction from raw transaction object")
-    let transactionObject = {
-        to: transaction.to,
-        nonce: transaction.nonce,
-        gasLimit: ethers.BigNumber.from(transaction.gasLimit),
-        gasPrice: ethers.BigNumber.from(transaction.gasPrice),
-        data: transaction.data,
-        value: ethers.BigNumber.from(transaction.value),
-        chainId: transaction.chainId
-    }
-
-    let signature = {
-        r: transaction.r!,
-        s: transaction.s,
-        v: transaction.v
-    }
-
-    let signedTransaction = ethers.utils.serializeTransaction(transactionObject, signature)
-
-    return signedTransaction
-}
-
-
 function createSandwichBundle(filling: string, executorWallet: Signer): Array<(FlashbotsBundleTransaction | FlashbotsBundleRawTransaction)> {
-    // const bundleTransactions = new Array<(FlashbotsBundleTransaction | FlashbotsBundleRawTransaction)>(
-    //     {
-    //         transaction: topBread,
-    //         signer: executorWallet
-    //     },
-    //     {
-    //         signedTransaction: filling
-    //     },
-    //     {
-    //         transaction: bottomBread,
-    //         signer: executorWallet
-    //     },
-    // )
     const bundleTransactions = new Array<(FlashbotsBundleTransaction | FlashbotsBundleRawTransaction)>(
         {
             signedTransaction: filling
@@ -75,39 +29,43 @@ function createSandwichBundle(filling: string, executorWallet: Signer): Array<(F
 }
 
 async function main() {
-    const flashbotsProvider = await FlashbotsBundleProvider.create(provider, flashbotsRelaySigningWallet)
+    const flashbotsProvider = await FlashbotsBundleProvider.create(provider, flashbotsRelaySigningWallet, process.env.FLASHBOTS_GOERLI_ENDPOINT, NETWORK)
     const scanTxPool = () => {
         provider.on("pending", async (txHash) => {
             const blockNumber = await provider.getBlockNumber();
-            const targetTxResponse = await provider.getTransaction(txHash);
-            if(targetTxResponse) {
-                const signedTx = await getSignedTransaction(targetTxResponse);
+            const txResponse = await provider.getTransaction(txHash);
+            if(txResponse) {
+                console.log("sender", txResponse.from);
+                console.log(txResponse);
+                const signedTx = await getSignedTransaction(txResponse);
                 // TODO: make gas fee variable
 
                 // create bundle
                 const bundledTransactions = createSandwichBundle(signedTx , arbitrageSigningWallet);
                 console.log(bundledTransactions)
                 console.log("bundle created")
+
                 // signed bundle
                 const signedBundle = await flashbotsProvider.signBundle(bundledTransactions);
+
                 // run simulation
                 const simulation = await flashbotsProvider.simulate(signedBundle, blockNumber + 1);
-                console.log(simulation)
-                if ("error" in simulation) {
-                    throw new Error(simulation.error.message)
-                }
-                if(simulation.firstRevert !== undefined) {
-                    console.log("simulation.firstRevert:", simulation.firstRevert)
-                    process.exit(1)
-                }
+                if ('error' in simulation) {
+                    // throw new Error(`Simulation Error: ${simulation.error.message}`)
+                    console.warn(`Simulation Error: ${simulation.error.message}`)
+                    return
+                  } else {
+                    console.log(`Simulation Success: ${JSON.stringify(simulation, null, 2)}`)
+                  }
+
                 // submit bundle
                 const bundleSubmitResponse = await flashbotsProvider.sendRawBundle(signedBundle, blockNumber + 1)
-
                 if ("error" in bundleSubmitResponse) {
-                    throw new Error(bundleSubmitResponse.error.message)
+                    console.warn(bundleSubmitResponse.error.message)
+                    return
                 }
                 else {
-                    console.log("submitted") 
+                    console.log("submitted")
                 }
             }
          })
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 import { BigNumber, Wallet, ethers } from "ethers";
-import { TransactionRequest, TransactionResponse} from "@ethersproject/abstract-provider";
+import { TransactionResponse } from "@ethersproject/abstract-provider";
 
 export const ETHER = BigNumber.from(10).pow(18)
 export const GWEI = BigNumber.from(10).pow(9)
@@ -13,4 +13,27 @@ export function bigNumberToDecimal(value: BigNumber, decimals = 18): number {
 export function getDefaultRelaySigningKey(): string {
     console.warn("You have not specified an explicity FLASHBOTS_RELAY_SIGNING_KEY environment variable. Creating random signing key, this searcher will not be building a reputation for next run")
     return Wallet.createRandom().privateKey;
+}
+
+export async function getSignedTransaction(transaction: TransactionResponse) {
+    console.log("Getting signed transaction from raw transaction object")
+    let transactionObject = {
+        to: transaction.to,
+        nonce: transaction.nonce,
+        gasLimit: ethers.BigNumber.from(transaction.gasLimit),
+        gasPrice: ethers.BigNumber.from(transaction.gasPrice),
+        data: transaction.data,
+        value: ethers.BigNumber.from(transaction.value),
+        chainId: transaction.chainId
+    }
+
+    let signature = {
+        r: transaction.r!,
+        s: transaction.s,
+        v: transaction.v
+    }
+
+    let signedTransaction = ethers.utils.serializeTransaction(transactionObject, signature)
+
+    return signedTransaction
 }
